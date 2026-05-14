90ca3483eda8da05dd3f90645d3a6f3e80641da5
Daniel <est.daniela.fuentesb@unimilitar.edu.co>
2026-05-14 10:52:57 -0500

    Se crea el reto de recorre_arbol

---
+++ src/NodoArbol.java
@@ -0,0 +1,42 @@
+
+public class NodoArbol <T> {
+    private T dato;
+    private NodoArbol[] hijo;
+
+    public NodoArbol(T dato, NodoArbol[] hijo) {
+        this.dato = dato;
+        this.hijo = hijo;
+    }
+    NodoArbol(){}
+    public T getDato() {
+        return dato;
+    }
+
+    public void setDato(T dato) {
+        this.dato = dato;
+    }
+
+    public NodoArbol[] getHijo() {
+        return hijo;
+    }
+
+    public void setHijo(NodoArbol[] hijo) {
+        this.hijo = hijo;
+    }
+    public void preOrden(){
+        System.out.print(this.dato);
+        if(hijo == null) return;
+        for(NodoArbol n : hijo ){
+            System.out.print(", ");       
+        n.preOrden();
+        }
+    }
+  public void setHijo(int posicion, T dato){
+      if(hijo==null){
+          hijo= new NodoArbol[2];
+      }
+        hijo[posicion] = new NodoArbol(dato,null);
+    }
+//System.out.println(","+n.dato);
+    
+}
